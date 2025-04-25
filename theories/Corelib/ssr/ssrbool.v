ed817be63c9502b24fff5bd92956ad3cce0bb7f8
Quentin Vermande <quentin.vermande@orange.fr>
2025-04-25 16:22:10 +0200

    mk pred_of_argType a coercion from Sortclass

--- theories/Corelib/ssr/ssrbool.v
+++ theories/Corelib/ssr/ssrbool.v
@@ -1367,7 +1367,7 @@ Module Export PredSortOfSimplCoercion := DeclarePredSortOfSimpl PredOfSimpl.
 Definition predArgType := Type.
 Bind Scope type_scope with predArgType.
 Identity Coercion sort_of_predArgType : predArgType >-> Sortclass.
-Coercion pred_of_argType (T : predArgType) : simpl_pred T := predT.
+Coercion pred_of_argType (T : Type) : simpl_pred T := predT.
 Notation "{ : T }" := (T%type : predArgType) : type_scope.
 
 (** Boolean relations.
