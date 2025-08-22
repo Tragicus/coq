3f71a730601af0c43302d339bd45d4b62772cbc4
Quentin Vermande <quentin.vermande@orange.fr>
2025-08-22 16:42:21 +0200

    more reductions after tc + debug

--- pretyping/evarconv.ml
+++ pretyping/evarconv.ml
@@ -754,6 +754,7 @@ and evar_eqappr_x ?(rhs_is_already_stuck = false) flags env evd pbty
     hds (* Copy of the never-reduced appr1 and appr2 *)
     lastUnfolded (* tells which side was last unfolded, if any *)
     (term1, sk1 as appr1) (term2, sk2 as appr2) =
+  let t = Random.int 1073741823 in
   let quick_fail i = (* not costly, loses info *)
     UnifFailure (i, NotSameHead)
   in
@@ -985,7 +986,10 @@ and evar_eqappr_x ?(rhs_is_already_stuck = false) flags env evd pbty
     let tc evd =
       let (e, _) = EConstr.destEvar evd termF in
       try let evd = get_tc evd e in
-        evar_eqappr_x flags env evd pbty hds lastUnfolded (whd_betaiota_deltazeta_for_iota_state flags.open_ts env evd apprF) apprR
+        let apprF = whd_betaiota_deltazeta_for_iota_state flags.open_ts env evd apprF in
+        let apprR = whd_betaiota_deltazeta_for_iota_state flags.open_ts env evd apprR in
+        let () = debug_unification Pp.(fun () -> int t ++ str " get_tc succeeds with " ++ pr_state env evd apprF ++ cut ()) in
+        evar_eqappr_x flags env evd pbty hds lastUnfolded apprF apprR
       with _ -> quick_fail evd in
     match Stack.list_of_app_stack skF with
     | None ->
@@ -1075,17 +1079,11 @@ and evar_eqappr_x ?(rhs_is_already_stuck = false) flags env evd pbty
     | _, _ -> anomaly (Pp.str "Unexpected result from ise_stack2.")
   in
   let app_empty = match sk1, sk2 with [], [] -> true | _ -> false in
-  let () = debug_unification (fun () -> Pp.(v 0 (pr_state env evd appr1 ++ cut () ++ pr_state env evd appr2 ++ cut ()))) in
+  let () = debug_unification (fun () -> Pp.(v 0 (int t ++ str " " ++ pr_state env evd appr1 ++ cut () ++ pr_state env evd appr2 ++ cut ()))) in
   match (flex_kind_of_term flags env evd term1 sk1,
          flex_kind_of_term flags env evd term2 sk2) with
     | Flexible (sp1,al1), Flexible (sp2,al2) ->
-        let k1 = if lastUnfolded = Some true && not (Stack.not_purely_applicative sk1) then let (t, sk) = orig.left in flex_kind_of_term flags env evd t sk else Flexible (sp1, al1) in
-        let k2 = if lastUnfolded = Some false && not (Stack.not_purely_applicative sk2) then let (t, sk) = orig.right in flex_kind_of_term flags env evd t sk else Flexible (sp2, al2) in
-        begin match (k1, k2) with
-        | Flexible ev1, MaybeFlexible v2 -> flex_maybeflex true ev1 appr1 orig.right orig.right
-        | MaybeFlexible v1, Flexible ev2 -> flex_maybeflex false ev2 appr2 orig.left orig.left
-        | _, Rigid | Rigid, _ -> anomaly (Pp.str "flexible terms cannot fold to rigid ones")
-        | _ ->
+      let () = debug_unification Pp.(fun () -> int t ++ str " both sides are flexible") in
       (* Notations:
          - "sk" is a stack (or, more abstractly, an evaluation context, written E)
          - "ev" is an evar "?ev", more precisely an evar ?n with an instance inst
@@ -1124,22 +1122,30 @@ and evar_eqappr_x ?(rhs_is_already_stuck = false) flags env evd pbty
           | Success _ as x -> x
           | UnifFailure _ -> quick_fail i
         and f6 i =
+          let () = debug_unification Pp.(fun () -> int t ++ str " get_tc with both sides flexible") in
           try let evd = get_tc evd sp2 in
-            evar_eqappr_x flags env evd pbty hds lastUnfolded appr1 (whd_betaiota_deltazeta_for_iota_state flags.open_ts env evd appr2)
+            evar_eqappr_x flags env evd pbty hds lastUnfolded
+              (whd_betaiota_deltazeta_for_iota_state flags.open_ts env evd appr1)
+              (whd_betaiota_deltazeta_for_iota_state flags.open_ts env evd appr2)
           with _ -> try
             let evd = get_tc evd sp1 in
-            evar_eqappr_x flags env evd pbty hds lastUnfolded (whd_betaiota_deltazeta_for_iota_state flags.open_ts env evd appr1) appr2
+            evar_eqappr_x flags env evd pbty hds lastUnfolded
+              (whd_betaiota_deltazeta_for_iota_state flags.open_ts env evd appr1)
+              (whd_betaiota_deltazeta_for_iota_state flags.open_ts env evd appr2)
           with _ -> quick_fail i
         in
         ise_try evd [f1; f2; f3; f4; f5; f6]
 
     | Flexible ev1, MaybeFlexible v2 ->
-      flex_maybeflex true ev1 appr1 appr2 (if Stack.not_purely_applicative sk1 then appr2 else orig.right)
+      let () = debug_unification Pp.(fun () -> int t ++ str " flex maybeflex") in
+      flex_maybeflex true ev1 appr1 appr2 v2
 
     | MaybeFlexible vsk1, Flexible ev2 ->
-      flex_maybeflex false ev2 appr2 appr1 (if Stack.not_purely_applicative sk2 then appr1 else orig.left)
+      let () = debug_unification Pp.(fun () -> int t ++ str " maybeflex flex") in
+      flex_maybeflex false ev2 appr2 appr1 vsk1
 
     | MaybeFlexible (v1', sk1' as vsk1'), MaybeFlexible (v2', sk2' as vsk2') -> begin
+        let () = debug_unification Pp.(fun () -> int t ++ str " maybeflex maybeflex") in
         match EConstr.kind evd term1, EConstr.kind evd term2 with
         | LetIn (na1,b1,t1,c'1), LetIn (na2,b2,t2,c'2) ->
         let f1 i = (* FO *)
@@ -1258,6 +1264,7 @@ and evar_eqappr_x ?(rhs_is_already_stuck = false) flags env evd pbty
     end
 
     | Rigid, Rigid when EConstr.isLambda evd term1 && EConstr.isLambda evd term2 ->
+        let () = debug_unification Pp.(fun () -> int t ++ str " rigid rigid") in
         let (na1,c1,c'1) = EConstr.destLambda evd term1 in
         let (na2,c2,c'2) = EConstr.destLambda evd term2 in
         ise_and evd
@@ -1270,23 +1277,14 @@ and evar_eqappr_x ?(rhs_is_already_stuck = false) flags env evd pbty
            (fun i -> exact_ise_stack2 env i (evar_conv_x flags) sk1 sk2)]
 
     | Flexible ev1, Rigid ->
-        if Stack.not_purely_applicative sk1 then flex_rigid true ev1 appr1 appr2 else
-        let (t2, sk2) as appr2 = orig.right in
-        begin match flex_kind_of_term flags env evd t2 sk2 with
-        | Flexible ev2 -> anomaly (Pp.str "rigid terms can not fold to flexible ones")
-        | MaybeFlexible v2 -> flex_maybeflex true ev1 appr1 appr2 appr2
-        | Rigid -> flex_rigid true ev1 appr1 appr2
-        end
+        let () = debug_unification Pp.(fun () -> int t ++ str " flex rigid") in
+        flex_rigid true ev1 appr1 appr2
     | Rigid, Flexible ev2 ->
-        if Stack.not_purely_applicative sk2 then flex_rigid false ev2 appr2 appr1 else
-        let (t1, sk1) as appr1 = orig.left in
-        begin match flex_kind_of_term flags env evd t1 sk1 with
-        | Flexible ev1 -> anomaly (Pp.str "rigid terms can not fold to flexible ones")
-        | MaybeFlexible v1 -> flex_maybeflex false ev2 appr2 appr1 appr1
-        | Rigid -> flex_rigid false ev2 appr2 appr1
-        end
+        let () = debug_unification Pp.(fun () -> int t ++ str " rigid flex") in
+        flex_rigid false ev2 appr2 appr1
 
     | MaybeFlexible vsk1', Rigid ->
+        let () = debug_unification Pp.(fun () -> int t ++ str " maybeflex rigid") in
         let f3 i =
            if (not flags.with_cs) || lastUnfolded = Some true then UnifFailure (i,NoCanonicalStructure)
            else
@@ -1303,6 +1301,7 @@ and evar_eqappr_x ?(rhs_is_already_stuck = false) flags env evd pbty
           ise_try evd [f3; f4]
 
     | Rigid, MaybeFlexible vsk2' ->
+        let () = debug_unification Pp.(fun () -> int t ++ str " rigid maybeflex") in
         let f3 i =
            if (not flags.with_cs) || lastUnfolded = Some false then UnifFailure (i,NoCanonicalStructure)
            else
@@ -1325,6 +1324,7 @@ and evar_eqappr_x ?(rhs_is_already_stuck = false) flags env evd pbty
         eta_lambda env evd false term2 (term1,sk1)
 
     | Rigid, Rigid -> begin
+        let () = debug_unification Pp.(fun () -> int t ++ str " rigid rigid") in
         match EConstr.kind evd term1, EConstr.kind evd term2 with
 
         | Sort s1, Sort s2 when app_empty ->
